14baafe32d529c7d58aba6b1c68de7b76cccc22b
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-15 13:27:17 -0700

    Cleanup. Refactorreminders into component.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,21 +5,29 @@ function MainContent() {
     return (
         <div>
             <What />
-            <h2>Don't forget to buy:</h2>
-            <p>
-                <input type="checkbox" id="milk" />
-                <label for="milk">Milk</label>
-            </p>
-            <p>
-                <input type="checkbox" id="cocoa" />
-                <label for="cocoa" >Cocoa</label>
-            </p>
-            <p>
-                <input type="checkbox" id="gas" />
-                <label for="gas">Gas</label>
-            </p>
+            <ReminderCheckboxes />
         </div>
     )
+
+    function ReminderCheckboxes() {
+        return (
+            <div>
+                <h2>Don't forget to buy:</h2>
+                <p>
+                    <input type="checkbox" id="milk" />
+                    <label for="milk">Milk</label>
+                </p>
+                <p>
+                    <input type="checkbox" id="cocoa" />
+                    <label for="cocoa" >Cocoa</label>
+                </p>
+                <p>
+                    <input type="checkbox" id="gas" />
+                    <label for="gas">Gas</label>
+                </p>
+            </div>
+        )
+    }
 }
 
 export default MainContent;
