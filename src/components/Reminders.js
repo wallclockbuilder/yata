46f8e92d8b793a4da77cc78edeb610162985bfc6
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-15 16:07:30 -0700

    Refactor todo item into own component named Reminder

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -4,20 +4,19 @@ function Reminders() {
         return (
             <div>
                 <h2>Don't forget to buy:</h2>
-                <p>
-                    <input type="checkbox" id="milk" />
-                    <label for="milk">Milk</label>
-                </p>
-                <p>
-                    <input type="checkbox" id="cocoa" />
-                    <label for="cocoa" >Cocoa</label>
-                </p>
-                <p>
-                    <input type="checkbox" id="gas" />
-                    <label for="gas">Gas</label>
-                </p>
+                <Reminder name="milk" />
+                <Reminder name="cocoa" />
+                <Reminder name="gas" />
             </div>
         )
     }
 
+    function Reminder(props) {
+        return (
+            <p>
+                <input type="checkbox" id={props.name} />
+                <label for={props.name}>{props.name}</label>
+            </p>
+        )
+    }
     export default Reminders;
