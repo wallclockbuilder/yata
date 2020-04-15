9b739de2855b37ece7df6a5bdcd87fd58b36aa9e
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-15 07:08:16 -0700

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <div className="App">
+      //   <div className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h2>Welcome to React</h2>
+      //   </div>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <What />
     );
+    function What() {
+      return (
+        <div>
+          <h2>Count Up</h2>
+          <ul>
+            <li>Vwan</li>
+            <li>Tooo</li>
+            <li>Sree</li>
+          </ul>
+        </div>
+      )
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import What from 'what';
-// import App from './App';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  //a <App />,
-  <What />,
+  <App />,
   document.getElementById('root')
 );
-
-function What() {
-  return (
-    <div>
-      <h2>Hello Vworld</h2>
-      <ul>
-        <li>Van</li>
-        <li>Tooo</li>
-        <li>Sree</li>
-      </ul>
-    </div>
-  )
-}
