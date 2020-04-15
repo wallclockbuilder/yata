36089bd1805656a267382a3f4c3e7a5c50e29a22
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-15 13:23:47 -0700

    Add reminder checkboxes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,8 +3,23 @@ import What from "./What";
 
 function MainContent() {
     return (
-      <What />
-      )
+        <div>
+            <What />
+            <h2>Don't forget to buy:</h2>
+            <p>
+                <input type="checkbox" id="milk" />
+                <label for="milk">Milk</label>
+            </p>
+            <p>
+                <input type="checkbox" id="cocoa" />
+                <label for="cocoa" >Cocoa</label>
+            </p>
+            <p>
+                <input type="checkbox" id="gas" />
+                <label for="gas">Gas</label>
+            </p>
+        </div>
+    )
 }
 
 export default MainContent;
