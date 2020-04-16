955c8217ead6a5dddc36e1f6cda53036718cfeb6
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-16 14:50:28 -0700

    Cleanup. Extract reminderData to its own file.

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -1,15 +1,8 @@
 import React from "react";
 import Reminder from "./Reminder";
+import reminderData from "../reminderData";
 
-const data = [
-    {id: 0,  item:"milk"},
-    {id: 1, item: "cocoa"},
-    {id: 2, item: "gas"},
-    {id: 3, item: "petrol"},
-    {id: 4, item: "diesel"}
-]
-
-const reminderList = data.map(function(datum) {
+const reminderList = reminderData.map(function(datum) {
     return <Reminder name={datum.item} key={datum.id} />;
 })
 
---
+++ src/reminderData.js
@@ -0,0 +1,9 @@
+const reminderData = [
+    {id: 0,  item:"milk"},
+    {id: 1, item: "cocoa"},
+    {id: 2, item: "gas"},
+    {id: 3, item: "petrol"},
+    {id: 4, item: "diesel"}
+]
+
+export default reminderData;
