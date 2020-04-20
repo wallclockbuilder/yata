4c69e5895b74b563c792b47b1877e60f518e331a
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-20 14:47:51 -0700

    Add Checked/tickedd field to ReminderData.

--- src/reminderData.js
+++ src/reminderData.js
@@ -1,9 +1,9 @@
 const reminderData = [
-    {id: 0,  item:"milk"},
-    {id: 1, item: "cocoa"},
-    {id: 2, item: "gas"},
-    {id: 3, item: "petrol"},
-    {id: 4, item: "diesel"}
+    {id: 0,  item:"milk", ticked: false},
+    {id: 1, item: "cocoa", ticked: true},
+    {id: 2, item: "gas", ticked: false},
+    {id: 3, item: "petrol", ticked: true},
+    {id: 4, item: "diesel", ticked: false}
 ]
 
 export default reminderData;
