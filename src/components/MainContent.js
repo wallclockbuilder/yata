740a53d9959001fe836b913dc5f7f023c55523d3
Mawueli Adzoe <wallclockbuilder@googlemail.com>
2020-04-21 15:28:37 -0700

    Cleanup. Nuke dummy countdown component.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Countdown from "./Countdown";
 import Reminders from "./Reminders"
 
 function MainContent() {
     return (
         <div>
-            <Countdown />
             <Reminders />
         </div>
     )    
